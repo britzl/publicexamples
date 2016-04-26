5cdaf7a0efcc9d67afcb4e95852d7ce5fea8831d
Björn Ritzl <bjorn.ritzl@king.com>
2016-04-26 07:49:11 +0200

    Changed the fixed aspect ratio example to center or zoom out depending on window size

--- examples/fixed_aspect_ratio/render_helper.lua
+++ examples/fixed_aspect_ratio/render_helper.lua
@@ -1,13 +1,7 @@
 local M = {}
 
-M.zoom = true
-
 function M.action_to_position(action)
-	if M.zoom then
-		return vmath.vector3(action.screen_x * (M.zoom_factor or 1), action.screen_y * (M.zoom_factor or 1), 0)
-	else
-		return vmath.vector3(action.screen_x, action.screen_y, 0)
-	end
+	return vmath.vector3((M.xoffset or 0) + action.screen_x / (M.zoom_factor or 1), (M.yoffset or 0) + action.screen_y / (M.zoom_factor or 1), 0)
 end
 
 return M
