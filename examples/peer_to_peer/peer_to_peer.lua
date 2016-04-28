483280e09386f58b044c7d5dd8f144a1e78b5b4b
Björn Ritzl <bjorn.ritzl@king.com>
2016-04-28 14:14:45 +0200

    Updated peer to peer example

--- examples/peer_to_peer/peer_to_peer.lua
+++ examples/peer_to_peer/peer_to_peer.lua
@@ -6,53 +6,13 @@ local STATE_DISCONNECTED = "STATE_DISCONNECTED"
 local STATE_BROADCASTING = "STATE_BROADCASTING"
 local STATE_LISTENING = "STATE_LISTENING"
 
-local function create_udp_broadcast(port)
-	local udp_broadcast, err = socket.udp()
-	if not udp_broadcast then
-		return nil, err
+local function get_ip()
+	for _,network_card in pairs(sys.get_ifaddrs()) do
+		if network_card.up and network_card.address then
+			return network_card.address
+		end
 	end
-	
-	local ok, err = udp_broadcast:settimeout(0)
-	if not ok then
-		return nil, err
-	end
-	
-	local ok, err = udp_broadcast:setsockname("*", port)
-	if not ok then
-		return nil, err
-	end
-	
-	local ok, err = udp_broadcast:setoption("broadcast", true)
-	if not ok then
-		return nil, err
-	end
-
-	local ok, err = udp_broadcast:setpeername("255.255.255.255", port)
-	if not ok then
-		return nil, err
-	end
-	
-	return udp_broadcast
-end
-
-
-local function create_udp_listen(port)
-	local udp_listen, err = socket.udp()
-	if not udp_listen then
-		return nil, err
-	end
-	
-	local ok, err = udp_listen:settimeout(0)
-	if not ok then
-		return nil, err
-	end
-
-	local ok, err = udp_listen:setpeername("255.255.255.255", port)
-	if not ok then
-		return nil, err
-	end
-	
-	return udp_listen
+	return nil
 end
 
 
@@ -62,20 +22,33 @@ function M.create()
 	
 	local state = STATE_DISCONNECTED
 
+	local multicast_ip = "226.192.1.1"
+	local broadcast_port = 8100
+
 	local listen_co
 	local broadcast_co
 	
-	function instance.broadcast(port)
-		local udp_broadcast, err = create_udp_broadcast(port)
-		if not udp_broadcast then
+	function instance.broadcast(message)
+		assert(message, "You must provide a message to broadcast")
+		local broadcaster
+		local ok, err = pcall(function()
+			broadcaster = socket.udp()
+			broadcaster:settimeout(0)
+		end)
+		if not broadcaster then
 			return false, err
 		end
 		
+		print("Broadcasting " .. message) 
 		state = STATE_BROADCASTING
 		broadcast_co = coroutine.create(function()
 			while state == STATE_BROADCASTING do
-				--print("broadcasting")
-				local ok, err = udp_broadcast:send('this is a test')
+				local ok, err = pcall(function()
+					broadcaster:sendto(message, multicast_ip, broadcast_port)
+					broadcaster:setoption("broadcast", true)
+					broadcaster:sendto(message, "255.255.255.255", broadcast_port)
+					broadcaster:setoption("broadcast", false)
+				end)
 				if err then
 					state = STATE_DISCONNECTED
 				else
@@ -88,20 +61,36 @@ function M.create()
 		return coroutine.resume(broadcast_co)
 	end
 	
-	function instance.listen(port)
-		local udp_listen, err = create_udp_listen(port)
-		if not udp_listen then
+	function instance.listen(message)
+		assert(message, "You must provide a message to listen for")
+		local listener
+		local ok, err = pcall(function()
+			listener = socket.udp()
+			listener:setsockname("0.0.0.0", broadcast_port)
+		
+			-- try multicast
+			if listener:getsockname() then
+				listener:setoption("ip-add-membership", { multiaddr = multicast_ip, interface = get_ip() } )
+			-- fallback to broadcast
+			else
+				listener:close()
+				listener = socket.udp()
+				listener:setsockname(get_ip(), broadcast_port)
+			end
+			
+			listener:settimeout(0)
+		end)
+		if not listener then
 			return false, err
 		end
 		
+		print("Listening for " .. message)
 		state = STATE_LISTENING
 		listen_co = coroutine.create(function()
 			while state == STATE_LISTENING do
-				--print("listening")
-				local data, err = udp_listen:receive()
-				print(data, err)
-				if data then
-					print(data)
+				local data, ip, port = listener:receivefrom()
+				if data and data == message then
+					print("Found server on " .. ip .. ":" .. port)
 				end
 				coroutine.yield()
 			end
