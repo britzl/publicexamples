bf8fb33b9a71df747b9eb2bdbf07a29be28f80e6
Björn Ritzl <bjorn.ritzl@king.com>
2016-09-05 08:00:35 +0200

    Added repro case for DEF-2095

---
+++ examples/DEF-2095/spawn.go
@@ -0,0 +1,31 @@
+components {
+  id: "script"
+  component: "/examples/DEF-2095/spawn.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/examples/assets/player.atlas\"\ndefault_animation: \"blue_walk\"\nmaterial: \"/builtins/materials/sprite.material\"\nblend_mode: BLEND_MODE_ALPHA\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
