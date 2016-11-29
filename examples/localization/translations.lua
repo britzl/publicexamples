99795524b2382395a985e0251d61727eddc2329d
Björn Ritzl <bjorn.ritzl@king.com>
2016-11-29 07:38:21 +0100

    Added a localization example

---
+++ examples/localization/translations.lua
@@ -0,0 +1,12 @@
+-- create this by hand or generate it from an export from a localization tool of some kind
+-- optionally parse it from json
+return {
+	["en"] = {
+		GREETING = "Hello, my name is %s",
+		BYE = "Bye",
+	},
+	["se"] = {
+		GREETING = "Hej, jag heter %s",
+		BYE = "Hej då",
+	}
+}
