1596a23a76e6ab18698f9c835f5d4458c6620853
Björn Ritzl <bjorn.ritzl@king.com>
2017-04-19 10:50:18 +0200

    Improved localisation example

--- examples/localization/localization/translate.lua
+++ examples/localization/localization/translate.lua
@@ -1,9 +1,26 @@
-local translations = require "localization.translations"
-
 local M = {
 	default_language = "en",
-	current_language = sys.get_sys_info().device_language or sys.get_sys_info().language
+	current_language = sys.get_sys_info().device_language or sys.get_sys_info().language,
+	translations = {},
 }
+
+--- Add translations
+-- Translations should be in the format:
+--
+-- {
+--   [language] = {
+--     [key] = string,
+--   }
+-- }
+-- @param translations
+function M.add_translations(translations)
+	for language,translations_for_language in pairs(translations) do
+		M.translations[language] = M.translations[language] or {} 
+		for key,translation in pairs(translations_for_language) do
+			M.translations[language][key] = translation
+		end
+	end
+end
 
 --- Change current language
 -- @param language
@@ -11,13 +28,12 @@ function M.change_language(language)
 	M.current_language = language
 end
 
-
 --- Get translation for a specific key, optionally formatting it with
 -- additional values
 -- @param key The key containing the text to translate
 -- @return The translated text
 function M.translate(key, ...)
-	local t = translations[M.current_language] or translations[M.default_language]
+	local t = M.translations[M.current_language] or M.translations[M.default_language]
 	if not t or not t[key] then
 		return key
 	else
