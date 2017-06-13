9612acfe96c9fccc5c94e01b76771102984c3411
Björn Ritzl <bjorn.ritzl@king.com>
2017-06-13 09:44:48 +0200

    Final version of Mike Dangers/Climberz game

--- examples/mike_dangers/game/diamond.go
+++ examples/mike_dangers/game/diamond.go
@@ -1,3 +1,18 @@
+components {
+  id: "particlefx"
+  component: "/game/diamondparticle.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
