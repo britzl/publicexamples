df0db15e4ee14dc7e4714d0e6c581a5214e2e469
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-03-21 20:56:27 +0100

    Added 3d example with collisions

---
+++ examples/threedee/assets/materials/model.fp
@@ -0,0 +1,18 @@
+varying mediump vec4 var_position;
+varying mediump vec3 var_normal;
+varying mediump vec2 var_texcoord0;
+varying mediump vec4 var_light;
+
+uniform lowp sampler2D tex0;
+uniform lowp vec4 tint;
+uniform lowp vec4 light;
+
+void main()
+{
+    // Pre-multiply alpha since all runtime textures already are
+    vec4 tint_pm = vec4(tint.xyz * tint.w, tint.w);
+    vec4 color = texture2D(tex0, var_texcoord0.xy) * tint_pm;
+
+    gl_FragColor = vec4(color.rgb,1.0);
+}
+
---
+++ examples/threedee/threedee/cube.go
@@ -0,0 +1,62 @@
+components {
+  id: "cube"
+  component: "/assets/models/cube.model"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.9\n"
+  "restitution: 0.8\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 1.0\n"
+  "  data: 1.0\n"
+  "  data: 1.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
