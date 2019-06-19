6a44b81f59ae779e3ef80c38a48660a7a7417824
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-06-19 06:28:11 +0200

    Cleanup of bullet script

--- examples/top_down_spaceship/top_down_spaceship/bullet.go
+++ examples/top_down_spaceship/top_down_spaceship/bullet.go
@@ -1,18 +1,3 @@
-components {
-  id: "script"
-  component: "/top_down_spaceship/bullet.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
